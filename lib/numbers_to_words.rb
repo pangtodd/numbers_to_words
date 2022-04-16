7c3191a91076a57d1a1615d1715619bcceba8bc3
Todd Pangilinan <pang.todd@gmail.com>
2022-04-15 19:04:48 -0700

    started refactoring to include a class definition. Also will need to change approach, split does not work on ints.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,6 +1,8 @@
 require('pry') 
 
-def reverse(user_number)
- new_array=user_number.split
- new_array
+class Numbers_translator
+  def reverse(user_number)
+    new_array=user_number.split()
+    new_array
+  end
 end
