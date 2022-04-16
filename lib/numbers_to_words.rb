f6cd012ca65cc33ebe1af243bcb3182ddf638a4d
Todd Pangilinan <pang.todd@gmail.com>
2022-04-15 19:33:45 -0700

    numbers_in_english passes first test for single digits.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,8 +1,8 @@
 require('pry') 
 
 class Numbers_translator
-  def reverse(user_number)
-    new_array=user_number.split()
-    new_array
+  SINGLE_DIGITS= {1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine"}
+  def numbers_in_english(user_number)
+    SINGLE_DIGITS.fetch(user_number)
   end
 end
