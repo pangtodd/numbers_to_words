9326d3a457acd4142dfe92d31b3ca800e4e823d5
Todd Pangilinan <pang.todd@gmail.com>
2022-04-15 19:10:08 -0700

    refactored numbers_to_words_spec.rb to reflect class. Also re-wrote test one for numbers_in_english.

--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -1,8 +1,11 @@
 require('rspec')
 require('numbers_to_words')
 
-describe('#reverse') do
-  it("takes a number and makes it into an array") do
-    expect(reverse(2)).to eq([2])
+describe(Numbers_translator)do
+  describe('#reverse') do
+    it("takes a number and makes it into an array") do
+      test = Numbers_translator.new
+      expect(test.reverse(12)).to eq([1,2])
+    end
   end
 end
