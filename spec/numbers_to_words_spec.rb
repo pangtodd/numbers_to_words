c66ac04de149c91db196cd9b495e7a30967f27da
Todd Pangilinan <pang.todd@gmail.com>
2022-04-16 07:34:11 -0700

    establish test 2, converting teen numbers to english

--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -3,9 +3,12 @@ require('numbers_to_words')
 
 describe(Numbers_translator)do
   describe('#numbers_in_english') do
-    it("takes a number and returns it in english") do
+    it("takes a single-digit number and returns it in english") do
       test = Numbers_translator.new
-      expect(test.numbers_in_english(8)).to eq("nine")
+      expect(test.numbers_in_english(9)).to eq("nine")
     end
+    it("takes a teen number and returns it in english") do
+      test = Numbers_translator.new
+      expect(test.numbers_in_english(17)).to eq("seventeen")
   end
 end
