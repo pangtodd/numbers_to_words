c520c77e18e121e2468cab242deac539f904b3b5
Todd Pangilinan <pang.todd@gmail.com>
2022-04-15 20:38:56 -0700

    WIP.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -1,8 +1,12 @@
 require('pry') 
 
 class Numbers_translator
-  SINGLE_DIGITS= {1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine"}
+  SINGLE_DIGITS= {0=>"",1=>"one", 2=>"two", 3=>"three", 4=>"four", 5=>"five", 6=>"six", 7=>"seven", 8=>"eight", 9=>"nine"}
+  TEENS = {10=>S"ten", 11=>"eleven", 12=>"tweleve", 13=>"thirteen",14=>"fourteen", 15=>"fifteen",16=>"sixteen", 17=>"seventeen", 18=>"eighteen", 19=>"ninteen"}
+  DECAS = {2=>"twenty", 3=>"thirty", 4=>"fourty", 5=>"fifty", 6=>"sixty",7=>"seventy" 8= "eighty", 9=>"ninety"}GIT 
   def numbers_in_english(user_number)
     SINGLE_DIGITS.fetch(user_number)
   end
 end
+
+
--- spec/numbers_to_words_spec.rb
+++ spec/numbers_to_words_spec.rb
@@ -2,10 +2,10 @@ require('rspec')
 require('numbers_to_words')
 
 describe(Numbers_translator)do
-  describe('#reverse') do
-    it("takes a number and makes it into an array") do
+  describe('#numbers_in_english') do
+    it("takes a number and returns it in english") do
       test = Numbers_translator.new
-      expect(test.reverse(12)).to eq([1,2])
+      expect(test.numbers_in_english(8)).to eq("nine")
     end
   end
 end
