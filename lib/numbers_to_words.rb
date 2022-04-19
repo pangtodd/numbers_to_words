dfc9621b5e58088069b469cbe3083a9280c717e4
Todd Pangilinan <pang.todd@gmail.com>
2022-04-18 23:13:40 -0700

    trying to refactor 1-100. Got single digits to pass test/work.

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -3,18 +3,23 @@ require('pry')
 class Numbers_translator
   SINGLE_DIGITS= {"0"=>"","1"=>"one", "2"=>"two", "3"=>"three", "4"=>"four", "5"=>"five", "6"=>"six", "7"=>"seven", "8"=>"eight", "9"=>"nine"}
   
-  TEENS = {"10"=>"ten", "11"=>"eleven", "12"=>"tweleve", "13"=>"thirteen","14"=>"fourteen", "15"=>"fifteen","16"=>"sixteen", "17"=>"seventeen", "18"=>"eighteen", "19"=>"ninteen"}
+  # TEENS = {"10"=>"ten", "11"=>"eleven", "12"=>"tweleve", "13"=>"thirteen","14"=>"fourteen", "15"=>"fifteen","16"=>"sixteen", "17"=>"seventeen", "18"=>"eighteen", "19"=>"ninteen"}
   
-  DECAS = {"2"=>"twenty", "3"=>"thirty", "4"=>"fourty", "5"=>"fifty", "6"=>"sixty","7"=>"seventy", "8"=>"eighty", "9"=>"ninety"}
+  # DECAS = {"2"=>"twenty", "3"=>"thirty", "4"=>"fourty", "5"=>"fifty", "6"=>"sixty","7"=>"seventy", "8"=>"eighty", "9"=>"ninety"}
   
   def numbers_in_english(user_number)
-    string_number=user_number.to_s
+    string_number = user_number.to_s
+    ones = SINGLE_DIGITS.fetch(string_number[-1])
+    # teen = TEENS.fetch(string_number.slice(-2..))
+    # tens = DECAS.fetch(string_number[-2])
+    # hundreds = "#{SINGLE_DIGITS.fetch(string_number[-3])} hundred"
+    
     if user_number<10
-      SINGLE_DIGITS.fetch(string_number)
-    elsif user_number<20
-      TEENS.fetch(string_number)
-    elsif user_number<100
-      (DECAS.fetch(string_number[0]))+"-"+(SINGLE_DIGITS.fetch(string_number[1]))
+      "#{ones}"
+    # elsif user_number<100
+    #   (DECAS.fetch(string_number[0]))+"-"+(SINGLE_DIGITS.fetch(string_number[1]))
+    # elsif user_number<1000
+    # ()
     end
   end
 end
@@ -24,3 +29,14 @@ end
 #   teens.fetch.(user_number[1].concat(user_number[0])
 # else
 #   deca.fetch(user_number[1])+single_digit.fetch(user_number[0])
+# 1,000,000,000 = 10 digits
+# ones
+# tens
+# hundreds
+# thousands
+#   ten thousand
+   # hundred thousand
+# Millions
+    #ten Million
+    #hundred million
+# Billion
