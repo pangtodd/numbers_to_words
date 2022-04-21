d76b9fe2788c4a0309658f1739f0cfa1aa185fc3
Todd Pangilinan <pang.todd@gmail.com>
2022-04-20 17:21:50 -0700

    numbers_in_english passes test for numbers up to 119

--- lib/numbers_to_words.rb
+++ lib/numbers_to_words.rb
@@ -3,7 +3,7 @@ require('pry')
 class Numbers_translator
   SINGLE_DIGITS= {"nil" =>"", "0"=>"","1"=>"one", "2"=>"two", "3"=>"three", "4"=>"four", "5"=>"five", "6"=>"six", "7"=>"seven", "8"=>"eight", "9"=>"nine"}
   
-  TEENS = {"10"=>"ten", "11"=>"eleven", "12"=>"tweleve", "13"=>"thirteen","14"=>"fourteen", "15"=>"fifteen","16"=>"sixteen", "17"=>"seventeen", "18"=>"eighteen", "19"=>"ninteen"}
+  TEENS = {"10"=>"ten", "11"=>"eleven", "12"=>"tweleve", "13"=>"thirteen","14"=>"fourteen", "15"=>"fifteen","16"=>"sixteen", "17"=>"seventeen", "18"=>"eighteen", "19"=>"nineteen"}
   
   DECAS = {"2"=>"twenty", "3"=>"thirty", "4"=>"fourty", "5"=>"fifty", "6"=>"sixty","7"=>"seventy", "8"=>"eighty", "9"=>"ninety"}
   
@@ -19,6 +19,8 @@ class Numbers_translator
       "#{TEENS.fetch(string_number.slice(-2..))}"
     elsif user_number<100
       "#{DECAS.fetch(string_number[-2])}-#{ones}"
+    elsif user_number<999 && (string_number[-2]) == "1"
+      "#{SINGLE_DIGITS.fetch(string_number[-3])} hundred #{TEENS.fetch(string_number.slice(-2..))}"
     # elsif user_number<100
     #   (DECAS.fetch(string_number[0]))+"-"+(SINGLE_DIGITS.fetch(string_number[1]))
     # elsif user_number<1000
